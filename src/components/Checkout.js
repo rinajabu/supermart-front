3f1603bb78c61756c18b57b322caa1acd3dd8dbb
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-03 15:52:26 -0400

    checkout page styling done

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -9,17 +9,23 @@ const Checkout = (props) => {
     const [{cart}, dispatch] = useStateValue()
 
     return (
-        <div className="checkout">
-            <div className="checkout_products">
-                <h3>Your Cart Items</h3>
-                { cart.map(item => (
-                    <CheckoutProduct item={item} />
-                ))}
+        <>
+            <div className="checkout" id="checkout_top">
+                <div className="checkout_products">
+                    <h3>Your Cart Items</h3>
+                    { cart.length === 0 &&
+                        <p className="checkout_empty">Cart Is Empty...</p>
+                    }
+                    { cart.map(item => (
+                        <CheckoutProduct item={item} />
+                    ))}
+                </div>
+                <div className="checkout_subtotal">
+                    <Subtotal />
+                </div>
             </div>
-            <div className="checkout_subtotal">
-                <Subtotal />
-            </div>
-        </div>
+            <a className="checkout_backToTop" href="#checkout_top">Back to Top</a>
+        </>
     )
 }
 
