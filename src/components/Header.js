3343d170e170ebe556e40ffa48507b6b39a073b4
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-02 22:42:01 -0400

    styling halfway done

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,36 +2,66 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useStateValue } from '../StateProvider';
 import './Header.css';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import MoreVertSharpIcon from '@material-ui/icons/MoreVertSharp';
+import Dropdown from 'react-bootstrap/Dropdown'
+
 
 const Header = (props) => {
     
     const [{cart}, dispatch] = useStateValue();
 
     return (
-        <div className="header">
+        <div className="header sticky-top navbar">
             <Link to='/'>
-            <h1>SuperMart</h1>
+            <h1 className="header_title">SuperMart</h1>
             </Link>
             <div className="header_nav">
-                <div className="header_createForm">
-                    <Link to='/create'>
-                    <div className="header_createFormButton">Create</div>
-                    </Link>
-                </div>
+
                 <div className="header_cart">
                     <Link to='/checkout'>
                     <div className="header_cartButton">
-                        Cart: {cart?.length}
+                        <ShoppingCartIcon />{cart?.length}
                     </div>
                     </Link>
                 </div>
-                {/* WORKING ONLY ON BACK END AND ONLY TO LOCALHOST */}
-                <div className="header_login">
-                    <a href="http://localhost:8000/login" target="_blank">Login</a>
-                </div>
-                <div className="header_signup">
-                    <a href="http://localhost:8000/register" target="_blank">Sign Up</a>
-                </div>
+
+                <Dropdown className="header_dropdown">
+
+                    <Dropdown.Toggle className="header_dropdownMenu">
+                        <MoreVertSharpIcon />
+                    </Dropdown.Toggle>
+                        
+                    <Dropdown.Menu>
+
+                        <Dropdown.Item>
+                            <Link to='/create'>
+                            <div className="header_create">
+                                Create Product
+                            </div>
+                            </Link>
+                        </Dropdown.Item>
+
+                    {/* WORKING ONLY ON BACK END AND ONLY TO LOCALHOST */}
+                        <Dropdown.Item 
+                            href="http://localhost:8000/login" 
+                            target="_blank" 
+                            className="header_login"
+                        >
+                            Login
+                        </Dropdown.Item>
+
+                        <Dropdown.Item 
+                            href="http://localhost:8000/register" target="_blank" 
+                            className="header_signup"
+                        >
+                            Sign Up
+                        </Dropdown.Item>
+
+                    </Dropdown.Menu>
+                </Dropdown>
+                
             </div>
         </div>
     )
