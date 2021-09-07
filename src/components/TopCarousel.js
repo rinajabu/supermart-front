3f657b72e89466ad659a7d791513b3f3bf06721e
Rina Joy Abu <rinajoyyy@Rinas-MacBook-Pro.local>
2021-09-07 12:02:03 -0400

    image carousel added

--- src/components/TopCarousel.js
+++ src/components/TopCarousel.js
@@ -5,38 +5,42 @@ import Carousel from 'react-bootstrap/Carousel'
 const TopCarousel = (props) => {
     return (
     <Carousel>
-        <Carousel.Item>
+        <Carousel.Item className="topcarousel_item">
             <img
-            className="d-block w-100"
+            className="topcarousel_img"
             src="https://images.everydayhealth.com/images/diet-nutrition/all-about-bananas-nutrition-facts-health-benefits-recipes-and-more-rm-722x406.jpg"
             alt="First slide"
             />
-            {/* <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption> */}
+            <Carousel.Caption>
+            <div className="topcarousel_captionContainer">
+                <h3 className="topcarousel_caption">SuperMart</h3>
+                <p className="topcarousel_caption">Your Place to Shop Groceries</p>
+            </div>
+            </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
-            className="d-block w-100"
+            className="topcarousel_img"
             src="https://www.abc.net.au/news/image/893500-3x2-700x467.jpg"
             alt="Second slide"
             />
-            {/* <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption> */}
+            <Carousel.Caption>
+            <div className="topcarousel_captionContainer">
+                <h3 className="topcarousel_caption">Great Deals On All Produce</h3>
+            </div>
+            </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
-            className="d-block w-100"
-            src="https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/271157_2200-732x549.jpg"
+            className="topcarousel_img"
+            src="https://d2j6dbq0eux0bg.cloudfront.net/images/27824195/1386549657.jpg"
             alt="Third slide"
             />
-            {/* <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </Carousel.Caption> */}
+            <Carousel.Caption>
+            <div className="topcarousel_captionContainer">
+                <h3 className="topcarousel_caption">Fresh Produce Delivered Daily</h3>
+            </div>
+            </Carousel.Caption>
         </Carousel.Item>
     </Carousel>
     )
